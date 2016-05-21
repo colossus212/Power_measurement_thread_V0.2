0332e04d63cb85a888758c8906484ff35931adb6
wjw890912 <925295580@qq.com>
2016-05-21 08:50:22 +0800

    add htx117 weight system and ir decode

---
+++ Thread2NUC442/ADE7878_NUC422_Softwave/Weight/inc/weight.h
@@ -0,0 +1,32 @@
+ #ifndef __WEIGHT_H__
+#define __WEIGHT_H__
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+
+
+ void ConfigSWTimr0(void);
+ void ConfigHWADCHX711(void);
+ void ConfigHWPwm0(void);
+  void ConfigHW_IR_IRQ(void);
+
+
+
+
+
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+
+#endif
+
+
+
+
